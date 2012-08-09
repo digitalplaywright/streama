cac4d3bfe17e8b7e6e6d289a1f3358fcf1df0692
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2012-08-09 10:39:37 -0700

    made id of streama option the option value

--- lib/streama/streama_option.rb
+++ lib/streama/streama_option.rb
@@ -4,6 +4,8 @@ class StreamaOption
   field :name,  :type => Symbol
   field :value, :type => String
 
+  field :_id, :type => Symbol, default: ->{ name }
+
   embedded_in :streama_optionable, :polymorphic => true
 
 end
